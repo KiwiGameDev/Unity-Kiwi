9c116c5128c61b36f62cb4fe25cf7bd534ed60df
Kiwi <17229588+kiwigamedev@users.noreply.github.com>
2022-11-02 21:46:45 +0800

    Added Unity-Kiwi

---
+++ Packages/Kiwi/Runtime/Collections/SerializableDictionary/AutomaticKeysAttribute.cs
@@ -0,0 +1,8 @@
+namespace Kiwi.Collections.SerializableDictionary
+{
+	[System.AttributeUsage(System.AttributeTargets.Field)]
+	public class AutomaticKeysAttribute : System.Attribute
+	{
+		public AutomaticKeysAttribute() { }
+	}
+}
