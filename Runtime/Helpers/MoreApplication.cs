e503def84354ace7edffbc94cd5a96de29d5cad6
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2022-11-20 22:11:33 +0800

    Added IsQuitting in MoreApplication

---
+++ Runtime/Helpers/MoreApplication.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Kiwi.Helpers
+{
+	public static class MoreApplication
+	{
+#if UNITY_EDITOR
+		[InitializeOnEnterPlayMode]
+		static void EnterPlayMode(EnterPlayModeOptions options)
+		{
+			IsQuitting = false;
+		}
+#endif
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+		static void RunOnStart()
+		{
+			IsQuitting = false;
+			Application.quitting += Quit;
+		}
+
+		static void Quit()
+		{
+			IsQuitting = true;
+		}
+
+		public static bool IsQuitting { get; private set; }
+	}
+}
