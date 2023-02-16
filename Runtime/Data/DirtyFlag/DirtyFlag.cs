18c30f19f1a205b94b1b40d673bc8f0c77ad4cd9
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2023-02-16 18:03:44 +0800

    Added Dirty Flag

---
+++ Runtime/Data/DirtyFlag/DirtyFlag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kiwi.Data
+{
+    [Serializable]
+    public class DirtyFlag
+    {
+        [SerializeField] bool value;
+
+        public void SetValue(bool value = true)
+        {
+            this.value |= value;
+        }
+
+        public bool GetValue()
+        {
+            if (!value)
+                return false;
+
+            value = false;
+
+            return true;
+        }
+
+        public bool GetValueNoReset()
+        {
+            return value;
+        }
+    }
+}
