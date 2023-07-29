589e16680dfd69de7ec7d16d5722fcbfc4529121
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2023-07-29 21:35:59 +0800

    Implemented `ShowIf` Attribute

---
+++ Editor/Attributes/ShowIfAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kiwi.Editor.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    public class ShowIfAttribute : PropertyAttribute
+    {
+        public readonly string[] Conditions;
+        public readonly string EnumName;
+        public readonly int EnumValueIndex;
+
+        public ShowIfAttribute(params string[] conditions)
+        {
+            Conditions = conditions;
+        }
+
+        public ShowIfAttribute(string enumName, object enumValue)
+        {
+            EnumName = enumName;
+            EnumValueIndex = (int) enumValue;
+        }
+    }
+}
---
+++ Runtime/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Kiwi.Reflection.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static IEnumerable<FieldInfo> GetAllFields(this object target, Func<FieldInfo, bool> predicate)
+        {
+            if (target == null)
+            {
+                Debug.LogError("The target object is null. Check for missing scripts.");
+                yield break;
+            }
+
+            List<Type> types = GetSelfAndBaseTypes(target);
+
+            for (int i = types.Count - 1; i >= 0; i--)
+            {
+                IEnumerable<FieldInfo> fieldInfos = types[i]
+                    .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    .Where(predicate);
+
+                foreach (FieldInfo fieldInfo in fieldInfos)
+                {
+                    yield return fieldInfo;
+                }
+            }
+        }
+
+        public static IEnumerable<PropertyInfo> GetAllProperties(this object target, Func<PropertyInfo, bool> predicate)
+        {
+            if (target == null)
+            {
+                Debug.LogError("The target object is null. Check for missing scripts.");
+                yield break;
+            }
+
+            List<Type> types = GetSelfAndBaseTypes(target);
+
+            for (int i = types.Count - 1; i >= 0; i--)
+            {
+                IEnumerable<PropertyInfo> propertyInfos = types[i]
+                    .GetProperties(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    .Where(predicate);
+
+                foreach (var propertyInfo in propertyInfos)
+                {
+                    yield return propertyInfo;
+                }
+            }
+        }
+
+        public static IEnumerable<MethodInfo> GetAllMethods(this object target, Func<MethodInfo, bool> predicate)
+        {
+            if (target == null)
+            {
+                Debug.LogError("The target object is null. Check for missing scripts.");
+                yield break;
+            }
+
+            List<Type> types = GetSelfAndBaseTypes(target);
+
+            for (int i = types.Count - 1; i >= 0; i--)
+            {
+                IEnumerable<MethodInfo> methodInfos = types[i]
+                    .GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    .Where(predicate);
+
+                foreach (MethodInfo methodInfo in methodInfos)
+                {
+                    yield return methodInfo;
+                }
+            }
+        }
+
+        public static FieldInfo GetField(this object target, string fieldName)
+        {
+            return GetAllFields(target, f => f.Name.Equals(fieldName, StringComparison.Ordinal)).FirstOrDefault();
+        }
+
+        public static PropertyInfo GetProperty(this object target, string propertyName)
+        {
+            return GetAllProperties(target, p => p.Name.Equals(propertyName, StringComparison.Ordinal)).FirstOrDefault();
+        }
+
+        public static MethodInfo GetMethod(this object target, string methodName)
+        {
+            return GetAllMethods(target, m => m.Name.Equals(methodName, StringComparison.Ordinal)).FirstOrDefault();
+        }
+
+        public static Type GetListElementType(this Type listType)
+        {
+            return listType.IsGenericType
+                ? listType.GetGenericArguments()[0]
+                : listType.GetElementType();
+        }
+
+        /// <summary>
+        ///		Get type and all base types of target, sorted as following:
+        ///		<para />[target's type, base type, base's base type, ...]
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        static List<Type> GetSelfAndBaseTypes(object target)
+        {
+            List<Type> types = new()
+            {
+                target.GetType()
+            };
+
+            while (types.Last().BaseType != null)
+            {
+                types.Add(types.Last().BaseType);
+            }
+
+            return types;
+        }
+    }
+}
