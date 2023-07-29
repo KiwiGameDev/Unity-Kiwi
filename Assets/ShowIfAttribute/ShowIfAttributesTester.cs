5fc53e812b695ff99e277af716ceac2e8bbc98c6
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2023-07-29 21:35:59 +0800

    Implemented `ShowIf` Attribute

---
+++ Assets/ShowIfAttribute/ShowIfAttributesTester.cs
@@ -0,0 +1,47 @@
+﻿using Kiwi.Editor.Attributes;
+using UnityEngine;
+
+namespace Kiwi.Data
+{
+    [DisallowMultipleComponent]
+    public class ShowIfAttributesTester : MonoBehaviour
+    {
+        enum TestEnum
+        {
+            ValueA,
+            ValueB,
+            ValueC
+        }
+
+        [Header("Conditional Values")]
+        [SerializeField] TestEnum testEnum;
+        [SerializeField] int testInt;
+
+        [Header("Tests")]
+        [ShowIf(nameof(AlwaysTrue))]
+        [SerializeField] float alwaysShow;
+        [ShowIf(nameof(testEnum), TestEnum.ValueA)]
+        [SerializeField] int showIfValueA;
+        [ShowIf(nameof(testEnum), TestEnum.ValueB)]
+        [SerializeField] float showIfValueB;
+        [ShowIf(nameof(ShowIfLessThan10))]
+        [SerializeField] float showIfLessThan10;
+        [ShowIf(nameof(AlwaysTrue), nameof(ShowIfLessThan20))]
+        [SerializeField] float showIfLessThan20;
+
+        bool AlwaysTrue()
+        {
+            return true;
+        }
+
+        bool ShowIfLessThan10()
+        {
+            return testInt < 10;
+        }
+
+        bool ShowIfLessThan20()
+        {
+            return testInt < 20;
+        }
+    }
+}
---
+++ Packages/Kiwi/Editor/Attributes/ShowIfPropertyDrawer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Kiwi.Reflection.Extensions;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Kiwi.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(ShowIfAttribute))]
+    public class DrawIfPropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return CanShow(property) ? base.GetPropertyHeight(property, label) : 0f;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (CanShow(property))
+            {
+                EditorGUI.PropertyField(position, property);
+            }
+        }
+
+        bool CanShow(SerializedProperty serializedProperty)
+        {
+            SerializedObject serializedObject = serializedProperty.serializedObject;
+            ShowIfAttribute showIfAttribute = (ShowIfAttribute) attribute;
+
+            if (showIfAttribute.EnumName != null)
+            {
+                if (!EvaluateEnum(serializedObject, showIfAttribute))
+                    return false;
+            }
+
+            string[] conditions = showIfAttribute.Conditions;
+
+            if (conditions != null)
+            {
+                if (!EvaluatePredicates(conditions, serializedObject))
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool EvaluateEnum(SerializedObject serializedObject, ShowIfAttribute showIfAttribute)
+        {
+            SerializedProperty conditionalSerializedProp = serializedObject.FindProperty(showIfAttribute.EnumName);
+            int conditionalEnumValueIndex = conditionalSerializedProp.enumValueIndex;
+            int showIfEnumValueIndex = showIfAttribute.EnumValueIndex;
+
+            return conditionalEnumValueIndex == showIfEnumValueIndex;
+        }
+
+        static bool EvaluatePredicates(IEnumerable<string> conditions, SerializedObject serializedObject)
+        {
+            foreach (string conditionSerializePropName in conditions)
+            {
+                SerializedProperty conditionalSerializedProp = serializedObject.FindProperty(conditionSerializePropName);
+
+                if (conditionalSerializedProp != null)
+                {
+                    if (conditionalSerializedProp.propertyType == SerializedPropertyType.Boolean)
+                    {
+                        bool conditionalValue = conditionalSerializedProp.boolValue;
+
+                        if (!conditionalValue)
+                            return false;
+                    }
+                    else
+                    {
+                        Debug.LogError($"{conditionalSerializedProp.propertyType} is not supported.");
+                        return true; // Default to show on error
+                    }
+                }
+                else
+                {
+                    Object target = serializedObject.targetObject;
+                    MethodInfo conditionalMethodInfo = target.GetMethod(conditionSerializePropName);
+
+                    if (!(bool) conditionalMethodInfo.Invoke(target, null))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
