5c9d539dadc74f346813e60e631c7b06a70b4c3d
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2023-07-29 21:46:04 +0800

    Implemented `EnableIf` Attribute

---
+++ Assets/Attributes/EnableIfAttributesTester.cs
@@ -0,0 +1,47 @@
+﻿using Kiwi.Editor.Attributes;
+using UnityEngine;
+
+namespace Kiwi.Data
+{
+    [DisallowMultipleComponent]
+    public class EnableIfAttributesTester : MonoBehaviour
+    {
+        enum TestEnum
+        {
+            ValueA,
+            ValueB,
+            ValueC
+        }
+
+        [Header("Conditional Values")]
+        [SerializeField] TestEnum testEnum;
+        [SerializeField] int testInt;
+
+        [Header("Tests")]
+        [EnableIf(nameof(AlwaysTrue))]
+        [SerializeField] float alwaysShow;
+        [EnableIf(nameof(testEnum), TestEnum.ValueA)]
+        [SerializeField] int showIfValueA;
+        [EnableIf(nameof(testEnum), TestEnum.ValueB)]
+        [SerializeField] float showIfValueB;
+        [EnableIf(nameof(ShowIfLessThan10))]
+        [SerializeField] float showIfLessThan10;
+        [EnableIf(nameof(AlwaysTrue), nameof(ShowIfLessThan20))]
+        [SerializeField] float showIfLessThan20;
+
+        bool AlwaysTrue()
+        {
+            return true;
+        }
+
+        bool ShowIfLessThan10()
+        {
+            return testInt < 10;
+        }
+
+        bool ShowIfLessThan20()
+        {
+            return testInt < 20;
+        }
+    }
+}
