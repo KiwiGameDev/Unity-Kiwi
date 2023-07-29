2afbef0b78ae01062b3dbabc9b75cf2d354bdbad
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2023-07-29 22:45:50 +0800

    Implemented `Expandable` Attribute

---
+++ Assets/Attributes/ExpandableAttributesTester.cs
@@ -0,0 +1,14 @@
+﻿using Kiwi.Editor.Attributes;
+using UnityEngine;
+
+namespace Kiwi.Data
+{
+    [DisallowMultipleComponent]
+    public class ExpandableAttributesTester : MonoBehaviour
+    {
+        [SerializeField] int sampleInt;
+        [SerializeField, Expandable] float sampleFloat;
+        [SerializeField, Expandable] ShowIfAttributesTester showIfAttributesTester;
+        [SerializeField, Expandable] EnableIfAttributesTester enableIfAttributesTester;
+    }
+}
---
+++ Packages/Kiwi/Editor/Attributes/ExpandablePropertyDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kiwi.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(ExpandableAttribute))]
+    public class ExpandablePropertyDrawer : PropertyDrawer
+    {
+        static readonly uint FOLDOUT_ARROW_WIDTH = 16;
+
+        UnityEditor.Editor editor;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Rect propertyRect = position;
+            propertyRect.xMin += FOLDOUT_ARROW_WIDTH;
+
+            EditorGUI.PropertyField(propertyRect, property, label, true);
+
+            if (property.propertyType != SerializedPropertyType.ObjectReference)
+                return;
+            if (property.objectReferenceValue == null)
+                return;
+
+            // ReSharper disable once AssignmentInConditionalExpression
+            if (property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none))
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.BeginVertical(GUI.skin.box);
+
+                if (editor == null)
+                {
+                    UnityEditor.Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
+                }
+
+                editor.OnInspectorGUI();
+
+                EditorGUILayout.EndVertical();
+                EditorGUI.indentLevel--;
+            }
+        }
+    }
+}
